dc0f3df50762066f7f40ce651ab39aceeabbddd3
Krubeal <dkirubel@gmail.com>
2022-12-20 00:10:54 +0100

    +coc working +better colors

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,11 +21,32 @@ return require('packer').startup(
         -- use 'Mofiqul/dracula.nvim'
         -- use 'gosukiwi/vim-atom-dark'
         use 'sainnhe/sonokai'
-        
+        -- use 'navarasu/onedark.nvim'
+
+        -- vim.o.termguicolors = true
+        vim.cmd [[colorscheme sonokai]]
+
         ------------------
         -- Transparency --
         ------------------
         use 'tribela/vim-transparent'
+        
+        -----------------------------
+        -- Treesitter Highlighting --
+        -----------------------------
+        
+        use { -- Highlight, edit, and navigate code
+            'nvim-treesitter/nvim-treesitter',
+            run = function()
+            pcall(require('nvim-treesitter.install').update { with_sync = true })
+          end,
+         }
+
+         
+        use { -- Additional text objects via treesitter
+            'nvim-treesitter/nvim-treesitter-textobjects',
+            after = 'nvim-treesitter',
+        }
 
         -------------------------
         -- Progamming workflow --
@@ -61,5 +82,24 @@ return require('packer').startup(
               }
         
         -- github copilot --
-        -- use {"github/copilot.vim"}
+        -- use {"github/copilot.vim"
+        --
+        -- [[ Configure Treesitter ]]
+        -- See `:help nvim-treesitter`
+         require('nvim-treesitter.configs').setup {
+         --  Add languages to be installed here that you want installed for treesitter
+           ensure_installed = { 'c', 'cpp', 'lua', 'python', 'bash', 'help' },
+
+           highlight = { enable = true },
+           indent = { enable = true },
+           incremental_selection = {
+             enable = false,
+             keymaps = {
+               init_selection = '<c-space>',
+               node_incremental = '<c-space>',
+               scope_incremental = '<c-s>',
+               node_decremental = '<c-backspace>',
+             },
+           },
+         }
 end)
